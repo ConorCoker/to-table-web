0bd580617ac411ce3e1548f02cb9138d9b68dfda
Conor Coker <conorcoker@gmail.com>
2025-04-23 17:29:00 +0100

    Update cors on server.js to accept all just for debugging purposes

--- server.js
+++ server.js
@@ -8,11 +8,8 @@ const restaurantsRouter = require('./routes/restaurants');
 const rolesRouter = require('./routes/roles');
 const app = express();
 
-const allowedOrigins = ['http://localhost:3001', 'https://d1ti6emat1jvt2.cloudfront.net', 'https://master.dwj4nb9if0xc0.amplifyapp.com'];
-
-
 app.use(cors({
-    origin: allowedOrigins,
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
 }));
