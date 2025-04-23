518543cc6ec21871d6c82d429eec796af1f06117
Conor Coker <conorcoker@gmail.com>
2025-04-23 12:11:18 +0100

    Revert previous change in the way we format the private key as it was not working on aws

--- config/firebase.js
+++ config/firebase.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 if (!firebaseAdmin.apps.length) {
     const serviceAccount = {
         projectId: process.env.FIREBASE_PROJECT_ID,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     };
 
