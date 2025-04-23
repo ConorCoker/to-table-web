5a2c2a227c9228b3910fdf7447b38ccc9b63b25b
Conor Coker <conorcoker@gmail.com>
2025-04-23 17:18:47 +0100

    remove some debugging logs from firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -2,9 +2,6 @@ const firebaseAdmin = require('firebase-admin');
 // require('dotenv').config();
 
 if (!firebaseAdmin.apps.length) {
-    console.log('Project ID:', process.env.FIREBASE_PROJECT_ID);
-    console.log('Client Email:', process.env.FIREBASE_CLIENT_EMAIL);
-    console.log('Private Key:', process.env.FIREBASE_PRIVATE_KEY);
     const serviceAccount = {
         projectId: process.env.FIREBASE_PROJECT_ID,
         privateKey: process.env.FIREBASE_PRIVATE_KEY,
