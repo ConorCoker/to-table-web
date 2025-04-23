590cdbecd9bdfdb745ea99e2e456d6e487a34821
Conor Coker <conorcoker@gmail.com>
2025-04-24 00:51:12 +0100

    When we refactored the app to remove our backend server the logic for sending a notification to android was removed, however this funvtionality was at the early stages and did not yet take role into account, we now use google cloud functions to run some code that will send a notification to the relevant restraurant and relevant role

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,7 +1,8 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getFunctions } from "firebase/functions";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -10,9 +11,13 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const db = getFirestore(app);
+const functions = getFunctions(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export { db, functions, auth, storage };
